260660d2a648f9a5e62b4f67676b9e059b075735
antonlep <anton@FIL16975W.accdom.for.int>
2021-09-18 20:05:58 +0300

    Include login window

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
+from os import getenv
 from flask import Flask
-from flask import render_template
+from flask import redirect, render_template, request, session
 from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
