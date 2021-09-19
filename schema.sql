f92a8f84adabc197407e4de143baf905087670c0
antonlep <anton@FIL16975W.accdom.for.int>
2021-09-19 12:42:06 +0300

    Add basic database structure

--- schema.sql
+++ schema.sql
@@ -1,6 +1,53 @@
-CREATE TABLE users (
+CREATE TABLE Users (
     id SERIAL PRIMARY KEY,
     username TEXT UNIQUE,
     password TEXT,
     admin BOOLEAN
+);
+
+CREATE TABLE Courses (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE TextMaterial (
+    id SERIAL PRIMARY KEY,
+    course_id INTEGER REFERENCES Courses
+);
+
+CREATE TABLE CourseUsers (
+    id SERIAL PRIMARY KEY,
+    course_id INTEGER REFERENCES Courses,
+    user_id INTEGER REFERENCES Users
+);
+
+CREATE TABLE TextQuestions (
+    id SERIAL PRIMARY KEY,
+    course_id INTEGER REFERENCES Courses,
+    name TEXT,
+    content TEXT
+);
+
+CREATE TABLE TextAnswers (
+    id SERIAL PRIMARY KEY,
+    course_id INTEGER REFERENCES Courses,
+    user_id INTEGER REFERENCES Users,
+    question_id INTEGER REFERENCES TextQuestions,
+    answer TEXT,
+    time TIMESTAMP
+);
+
+CREATE TABLE MultipleChoiceQuestions (
+    id SERIAL PRIMARY KEY,
+    course_id INTEGER REFERENCES Courses,
+    question_text TEXT,
+    right_answer INTEGER
+);
+
+CREATE TABLE MultipleChoiceAnswers (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES Users,
+    question_id INTEGER REFERENCES MultipleChoiceQuestions,
+    answer INTEGER,
+    time TIMESTAMP
 );
